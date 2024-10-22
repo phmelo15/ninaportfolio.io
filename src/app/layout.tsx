a01f190905baf81030d4635d508e186484c05afa
Pedro <phmelo15@gmail.com>
2024-10-22 00:17:28 -0300

    Update name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Portfólio - Pedro",
+  title: "Portfólio - Nina",
   description: "Descrição, do meu portfólio",
 };
 
