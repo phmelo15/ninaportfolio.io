373480c1a2d519f3264fd3dba97705b840fa53b6
Pedro <phmelo15@gmail.com>
2024-10-21 23:55:40 -0300

    disable eslint

--- src/constants/Icons.ts
+++ src/constants/Icons.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 export const Icons = {
   ViewIcon: require("../assets/icons/ViewIcon.png"),
   Javascript: require("../assets/icons/javascript.png"),
